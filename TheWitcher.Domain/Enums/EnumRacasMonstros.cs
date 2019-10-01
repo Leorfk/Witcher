8560e1365316ee170eae5908bbe53cbdb809b5fc
Leonardo Ferreira <leonardorodrigues_f@hotmail.com>
2019-09-30 22:06:56 -0300

    Projeto CRUD criado

---
+++ TheWitcher.Domain/Enums/EnumRacasMonstros.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWitcher.Domain.Enums
+{
+    public enum EnumRacasMonstros
+    {
+        construtos,
+        draconideos,
+        espectros,
+        feras,
+        hibridos,
+        insectoides,
+        necrofagos,
+        ogroides,
+        amaldicoados,
+        relictos,
+        vampiros
+    }
+}
---
+++ TheWitcher.Domain/Models/Monstro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWitcher.Domain.Enums;
+
+namespace TheWitcher.Domain.Models
+{
+    public class Monstro
+    {
+        private int Id;
+        private string Nome;
+        private DateTime DataEncontro;
+        private EnumRacasMonstros Raca;
+        private decimal Recompensa;
+
+        public int id
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                this.Id = id;
+            }
+        }
+    }
+}
