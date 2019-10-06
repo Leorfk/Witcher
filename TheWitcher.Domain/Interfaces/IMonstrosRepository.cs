df1242456b221e5dc40e8c5fe2360c4a1fde893f
Leonardo Ferreira <leonardorodrigues_f@hotmail.com>
2019-10-06 10:16:21 -0300

    adição da interface de repositório

---
+++ TheWitcher.Domain/Interfaces/IMonstrosRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWitcher.Domain.Models;
+
+namespace TheWitcher.Domain.Interfaces
+{
+    public interface IMonstrosRepository
+    {
+        IEnumerable<Monstro> GetAll();
+        Monstro Get(int id);
+        Monstro Add(Monstro monstro);
+        bool Update(Monstro monstro);
+        void Remove(int id);
+    }
+}
---
+++ TheWitcher.Domain/Interfaces/IMonstrosService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWitcher.Domain.Models;
+
+namespace TheWitcher.Domain.Interfaces
+{
+    public interface IMonstrosService
+    {
+        Monstro Buscar(int id);
+        IEnumerable<Monstro> ListarTodos();
+        IEnumerable<Monstro> BuscarPorRaca(int raca);
+        IEnumerable<Monstro> BuscarPorRecompensa(double recompensa);
+        Monstro Registrar(Monstro monstro);
+        bool Deletar(int id);
+        void Atualizar(Monstro monstro);
+    }
+}
